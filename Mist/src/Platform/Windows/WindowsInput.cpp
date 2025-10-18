4cab91f807f76a67c87b63e1ec2403579349fb7b
moony47 <sfmoon47@gmail.com>
2025-10-18 16:23:35 +0100

    + Better Input inheritance system - decision made at compile time instead of during runtime as platform is already decided + Stopped tracking VS project files, they are to be created by premake + Stopped tracking .clang-format and ImGui.ini files as they are user specific

--- Mist/src/Platform/Windows/WindowsInput.cpp
+++ Mist/src/Platform/Windows/WindowsInput.cpp
@@ -1,37 +1,34 @@
 #include "mistpch.h"
-
-#include "WindowsInput.h"
+#include "Mist/Core/Input.h"
 
 #include "Mist/Core/Application.h"
 #include <GLFW/glfw3.h>
 
 namespace Mist {
 
-Input* Input::s_Instance = new WindowsInput();
-
-bool WindowsInput::IsKeyPressedImpl(int keycode) {
+bool Input::IsKeyPressed(int keycode) {
     auto window = static_cast<GLFWwindow*>(MIST_APP.GetWindow().GetNativeWindow());
     auto state = glfwGetKey(window, keycode);
     return state == GLFW_PRESS || state == GLFW_REPEAT;
 }
 
-bool WindowsInput::IsMouseButtonPressedImpl(int button) {
+bool Input::IsMouseButtonPressed(int button) {
     auto window = static_cast<GLFWwindow*>(MIST_APP.GetWindow().GetNativeWindow());
     auto state = glfwGetMouseButton(window, button);
     return state == GLFW_PRESS;
 }
 
-float WindowsInput::GetMouseXImpl() {
-    auto [x, y] = GetMousePositionImpl();
+float Input::GetMouseX() {
+    auto [x, y] = GetMousePosition();
     return x;
 }
 
-float WindowsInput::GetMouseYImpl() {
-    auto [x, y] = GetMousePositionImpl();
+float Input::GetMouseY() {
+    auto [x, y] = GetMousePosition();
     return y;
 }
 
-std::pair<float, float> WindowsInput::GetMousePositionImpl() {
+std::pair<float, float> Input::GetMousePosition() {
     auto window = static_cast<GLFWwindow*>(MIST_APP.GetWindow().GetNativeWindow());
     double x, y;
     glfwGetCursorPos(window, &x, &y);
